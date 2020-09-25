0b3c7658ce80a02af5a2930b9492f9f71bdab5da
Matthew Elbing <melbing1@pride.hofstra.edu>
2020-09-25 11:28:54 -0400

    add basic WHOIS api query

---
+++ extension/whoIsQueryAndParse.js
@@ -0,0 +1,12 @@
+/* Places a query to the whois API and parse the domain owner" */
+
+const url = "https://api.ip2whois.com/v1?key=free&domain="; //Generic API Query String
+
+function getRegistrationOf(domain) { //Gets JSON data about a domain from the pulic record
+	var completeUrl = url + domain; //Create a complete query with the domain function argument
+	request.open("GET", completeUrl, true); //Open an https connection for the given completed URL
+	request.onload = function () {
+		//Save API provided data in this function
+	}
+	request.send() //REquest data
+}
