d1417c6ff4d241bd3250da66247e12af48889038
Matthew Elbing <melbing1@pride.hofstra.edu>
2020-09-27 14:57:40 -0400

    Add entry point JS file

--- extension/whoIsQueryAndParse.js
+++ extension/whoIsQueryAndParse.js
@@ -2,7 +2,13 @@
 
 const url = "https://api.ip2whois.com/v1?key=free&domain="; //Generic API Query String
 
-function getRegistrationOf(domain) { //Gets JSON data about a domain from the pulic record
+browser.notification.create("Hello World", {
+	"type": "basic",
+	"title": "testing",
+	"message": "hello world"
+});
+
+function getRegistrationOf(domain) { //Gets JSON data about a domain from the public record
 	var completeUrl = url + domain; //Create a complete query with the domain function argument
 	request.open("GET", completeUrl, true); //Open an https connection for the given completed URL
 	request.onload = function () {
