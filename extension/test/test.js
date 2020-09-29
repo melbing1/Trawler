15fea3b3bae40bbdf5653e811c2b131046fe6d6c
Matthew Elbing <melbing1@pride.hofstra.edu>
2020-09-29 11:06:28 -0400

    Implement Unit Testing

---
+++ extension/test/test.js
@@ -0,0 +1,8 @@
+let assert = require('assert');
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('Should return 01 when the value is not present', function () {
+            assert( 1 === 1)
+        });
+    });
+});
